59daba93528e29fd1186a1e1c66161e52fcdc313
Свят <svatoslavlazovskij@MacBook-Pro.local>
2026-05-31 10:06:58 +0300

    fix express 5 cors configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -48,7 +48,6 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
